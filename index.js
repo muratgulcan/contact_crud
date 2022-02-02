36307c7117fb3cb48fb9ad90bfa8d0991b70e102
muratgulcan <76414291+muratgulcan@users.noreply.github.com>
2022-02-03 02:02:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(bodyParser.json());
 //define routes
 app.use('/api/contact',require('./routes/api/contact'))
 
-const PORT = 4200
+const PORT = 5000
 
 app.listen(PORT,()=>{console.log(`Server started on port ${PORT} `)})
